3e313710933dcd45d40afe515a3634b574b2b70c
ewongz <eugene321@gmail.com>
2025-01-18 16:14:32 -0800

    update main.py with db

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,37 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from . import routers
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+from api import routers
+from api.core.logging import get_logger, setup_logging
+from api.core.config import app_configs, settings
+from api.core.migrations import run_migrations
+
+setup_logging()
+logger = get_logger(__name__)
+run_migrations()
 
 app = FastAPI(title="Marathon Training Planner",
               docs_url="/docs",
               redoc_url="/")
+
+app.include_router(routers.router)
+
+@asynccontextmanager
+async def lifespan(_application: FastAPI) -> AsyncGenerator:
+    # Startup
+    yield
+    # Shutdown
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.CORS_ORIGINS,
+    allow_origin_regex=settings.CORS_ORIGINS_REGEX,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
+    allow_methods=("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"),
+    allow_headers=settings.CORS_HEADERS,
 )
-app.include_router(routers.router)
+
+@app.get("/healthcheck", include_in_schema=False)
+async def healthcheck() -> dict[str, str]:
+    return {"status": "ok"}
